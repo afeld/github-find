fb9223562c00383cdf7a21792f0838e3d3f0854e
Aidan Feldman <aidan.feldman@gmail.com>
2011-10-28 10:58:38 -0400

    show and hide input box on 'f'/ESC, and redirect when done

--- content-keydown.js
+++ content-keydown.js
@@ -1,8 +1,42 @@
-$(window).keydown(function(e){
-  if (e.which === 70){ // the 'f' key
+var searchBoxVisible = false,
+  $searchBox;
+
+function onSearchKeydown(e){
+  if (e.which === 13){ // the ENTER key
     var REPO_REGEX = new RegExp("github\.com/([^/]+/[^/]+)(/.*)?$"),
-      repo = window.location.href.match(REPO_REGEX)[1];
+      repo = window.location.href.match(REPO_REGEX)[1],
+      query = $(this).val();
     
-    window.location = 'https://github.com/search?type=Code&q=query&repo=' + encodeURIComponent(repo);
+    window.location = 'https://github.com/search?type=Code&q=' + encodeURIComponent(query) + '&repo=' + encodeURIComponent(repo);
+  }
+}
+
+$(window).keydown(function(e){
+  if (!searchBoxVisible && e.which === 70){ // the 'f' key
+    // show the search box
+    
+    if (!$searchBox){
+      // create the input field
+      $searchBox = $('<input type="text" style="position:fixed; font-size:3em;"/>')
+        .keydown(onSearchKeydown)
+        .appendTo('body');
+    }
+    
+    $searchBox
+      .show()
+      .position({
+        my: 'center',
+        at: 'center',
+        of: window
+      })
+      .focus();
+    
+    searchBoxVisible = true;
+    // don't register the 'f' keydown
+    e.preventDefault();
+    
+  } else if (searchBoxVisible && e.which === 27){ // the ESC key
+    $searchBox.hide();
+    searchBoxVisible = false;
   }
 });
