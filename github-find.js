a5fb7252e5ebc09e411e5f5ebf9ee59680df4cc3
Aidan Feldman <aidan.feldman@gmail.com>
2011-10-28 08:39:42 -0400

    show as browser action when viewing a repo

---
+++ github-find.js
@@ -0,0 +1,12 @@
+var REPO_REGEX = new RegExp("github\.com/[^/]+/[^/]+");
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+  // If we are inside of a repo
+  if (tab.url.match(REPO_REGEX)){
+    // ... show the page action.
+    chrome.pageAction.show(tabId);
+  }
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
