17c292b67d2700489dfee69814306b5c87a82676
Aidan Feldman <aidan.feldman@gmail.com>
2011-10-30 17:45:07 -0400

    change key-combo to CTL-SHIFT-f

--- content-keydown.js
+++ content-keydown.js
@@ -1,7 +1,7 @@
 var searchBoxVisible = false,
   $searchBox, $input;
 
-function onSearchKeydown(e){
+function onSearchKeypress(e){
   if (e.which === 13){ // the ENTER key
     var REPO_REGEX = new RegExp("github\.com/([^/]+/[^/]+)(/.*)?$"),
       repo = window.location.href.match(REPO_REGEX)[1],
@@ -11,42 +11,47 @@ function onSearchKeydown(e){
   }
 }
 
+function showSearchBox(){
+  // show the search box
+  
+  if (!$searchBox){
+    // create the input field
+    $searchBox = $('<div style="background-color:#E4E8EC; border: 1px solid #999;  border-radius:10px; box-shadow: 5px 8px 10px #999; padding:15px; position:fixed;"><input type="text" style="font-size:3em;"/></div>');
+    
+    $searchBox.appendTo('body');
+    
+    $input = $searchBox.find('input')
+      .blur(hideSearchBox)
+      .keypress(onSearchKeypress);
+  }
+  
+  $searchBox
+    .show()
+    .position({
+      my: 'center',
+      at: 'center',
+      of: window
+    });
+  
+  $input.focus();
+  
+  searchBoxVisible = true;
+}
+
 function hideSearchBox(){
   $searchBox.hide();
   searchBoxVisible = false;
 }
 
 
+$(window).keypress(function(e){
+  if (!searchBoxVisible && e.which === 6){ // CTL-SHIFT-f
+    showSearchBox();
+  }
+});
+
 $(window).keydown(function(e){
-  if (!searchBoxVisible && e.which === 70){ // the 'f' key
-    // show the search box
-    
-    if (!$searchBox){
-      // create the input field
-      $searchBox = $('<div style="background-color:#E4E8EC; border: 1px solid #999;  border-radius:10px; box-shadow: 5px 8px 10px #999; padding:15px; position:fixed;"><input type="text" style="font-size:3em;"/></div>');
-      
-      $searchBox.appendTo('body');
-      
-      $input = $searchBox.find('input')
-        .blur(hideSearchBox)
-        .keydown(onSearchKeydown);
-    }
-    
-    $searchBox
-      .show()
-      .position({
-        my: 'center',
-        at: 'center',
-        of: window
-      });
-    
-    $input.focus();
-    
-    searchBoxVisible = true;
-    // don't register the 'f' keydown
-    e.preventDefault();
-    
-  } else if (searchBoxVisible && e.which === 27){ // the ESC key
+  if (searchBoxVisible && e.which === 27){ // the ESC key
     hideSearchBox();
   }
 });
